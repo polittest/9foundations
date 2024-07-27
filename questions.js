605778f27e9cb16e6a3aeb15ffc649f0d2dcd366
xDMich <149896755+xDMich@users.noreply.github.com>
2024-07-27 18:36:57 +0200

    Update questions.js

--- questions.js
+++ questions.js
@@ -54,17 +54,17 @@ questions =
         [//4
             "Showing respect to an unjust authority is a foolish act.",
 
-"Liberty is more important than security.",
+"Ideally, no individual would ever be required to serve any collective expectation.",
 
 "When tyranny becomes law, resistance becomes duty.",
 
-"People are divided into rulers and ruled ones.",
+"It's better to live a short but free life, than to live a long life with very little freedom.",
 
-"Slavery is morally wrong.",
+"Forced labour, even as a punishment is morally wrong.",
 
 "Victimless crimes should not be crimes.",
 
-"Rulership which is determined by popular consent is universally preferable to rulership which is appointed from the top down instead.",
+"Rulership which is determined by popular consent is universally preferable to rulership which is appointed by other ways.",
 
 "Citizens should never be denied the right to nonviolently assemble and protest in public spaces, even when it is likely to progress into rioting.",
 
@@ -95,7 +95,26 @@ questions =
             
         ]],[//In-group-Out-group
         [//4
-            
+                    "It is important for us to preserve pride in our traditions and heritage.",
+
+"It is important for families to stick together and uphold unity, even when its members do not get along.",
+
+"Pursuing the goal of raising a family, and a career that helps achieve this goal is more noble than pursuing a child-free life path.",
+
+"It is important for countries to pursue a level of economic and military self-sufficiency to avoid dependency on others.",
+
+"The primary goal of governments should be to prioritize the interests of their country and those who currently live in it, rather than pursue the wellbeing of humanity at large.",
+
+"A society works best when most of its members share similar values, beliefs, social norms, and mannerisms, with newcomers being expected to assimilate/adopt these values before joining.",
+
+"People should willing to sacrifice themselves for their community",
+
+"Even if all nationalities and ethnicities are equal, it would be better if each of them stayed in their own communities.",
+
+
+"If I had to choose to help someone in my family and someone outside my family, no matter who they are, I would rather choose person from my family",
+
+"Patriotism is a virtue."   
         ],
         [//3
             
@@ -169,7 +188,26 @@ questions =
      
         ]],[//Chastity-Degradation
         [//4
-            
+            "Chastity is an important virtue.",
+
+"People should not do things that are disgusting, even if no one is harmed.",
+
+"I care about the standards of decency and purity.",
+
+"It is shameful to purchase something purely based on its luxury status or trendiness.",
+
+"Human body is sacred and shouldn't be mistreated without important reason.",
+
+"Casual sex between people who are not in commited romantic relationships should be heavily discouraged.",
+
+"Although taste in art, architecture, and style is technically subjective: it is important that a society tries to uphold certain standards of beauty and sophistication.",
+
+"The act of incest should be prohibited, even if it is certain that the child will not be born and the act takes place in private.",
+
+
+"Necrophilia should be prohibited even if the deceased person consented to it before death, because it's disgusting and is act of lack respect for the family and friends of the deceased.",
+
+"A libertine lifestyle often leads to the degradation of a person."
 
         ],
         [//3
@@ -203,7 +241,7 @@ questions =
 
 "If I were a soldier and disagreed with my commanding officer’s orders, I would obey anyway because that is my duty.",
 
-"People are divided into rulers and ruled ones.",
+"People should be divided into rulers and ruled ones.",
 
 "The most important thing for children to learn is to accept discipline and authority of their parents.",
 
